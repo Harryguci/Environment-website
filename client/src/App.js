ef20b4d1eab91fc7a9f54bd63d66e3eab972dae4
Harryguci <huyquang0028@gmail.com>
2023-08-11 17:22:14 +0700

    Added Loading screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
-import React, { useState, useEffect } from "react";
+import React, { Suspense, useState, useEffect, useCallback } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./Assets/SCSS/index.scss";
@@ -57,31 +57,48 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const LoadingPage = useCallback(() => (
+    <div
+      style={{
+        width: "100vw",
+        height: 100 + "vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#0f1630",
+      }}
+    >
+      <h1 style={{ color: "white", fontSize: '10rem' }}>Loading...</h1>
+    </div>
+  ), []);
+
   return (
     <div className="App">
       <AuthContext.Provider value={{ authState, setAuthSate }}>
         <CartContext.Provider value={{ cartState, setCartState }}>
           <BrowserRouter>
-            <Routes>
-              <Route path="/login" element={<Login typeForm={"login"} />} />
-              <Route path="/signup" element={<Login typeForm={"signup"} />} />
-              <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/blogs" element={<Blogs />} />
-                <Route path="/blogs/single/:id" element={<BlogSingle />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/products/single/:id" element={<ProductSingle />} />
-                <Route path="/account/:tab" element={<Account />} />
-                <Route path="/account" element={<Account />} />
-                <Route path="/search/:q" element={<Search />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="*" element={<NoPage />} />
-              </Route>
-            </Routes>
+            <Suspense fallback={<LoadingPage />}>
+              <Routes>
+                <Route path="/login" element={<Login typeForm={"login"} />} />
+                <Route path="/signup" element={<Login typeForm={"signup"} />} />
+                <Route path="/" element={<Layout />}>
+                  <Route index element={<Home />} />
+                  <Route path="/" element={<Home />} />
+                  <Route path="/blogs" element={<Blogs />} />
+                  <Route path="/blogs/single/:id" element={<BlogSingle />} />
+                  <Route path="/products" element={<Products />} />
+                  <Route path="/products/single/:id" element={<ProductSingle />} />
+                  <Route path="/account/:tab" element={<Account />} />
+                  <Route path="/account" element={<Account />} />
+                  <Route path="/search/:q" element={<Search />} />
+                  <Route path="/search" element={<Search />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/cart" element={<Cart />} />
+                  <Route path="/contact" element={<Contact />} />
+                  <Route path="*" element={<NoPage />} />
+                </Route>
+              </Routes>
+            </Suspense>
           </BrowserRouter>
         </CartContext.Provider>
       </AuthContext.Provider>
